6d843025a0bc48bbe7036a77d06ff1671e8909aa
Niki <nikitaf40@gmail.com>
2016-09-23 05:04:10 +0300

    Learning java from zero

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by nikit on 23.09.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
